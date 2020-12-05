eac19659bf2f789e56cf59326b6434e1cc55e2d4
Israel Mamani Villanueva <69133942+GalletaPVP@users.noreply.github.com>
2020-12-05 16:15:24 -0500

    Add files via upload

---
+++ js/algoritmo.js
@@ -0,0 +1,61 @@
+function calculo(){
+    var tipo_prestamo = document.getElementById('tipo_prestamo');
+    var valor = tipo_prestamo.options[tipo_prestamo.selectedIndex].text;
+    var porcentaje=0;
+    if(valor === 'Hipotecario'){
+        document.getElementById('porcentaje').value = 0.13;
+        document.getElementById('porcentaje').innerText = '13%';
+    }else if(valor === 'Vivienda'){
+        document.getElementById('porcentaje').value = 0.12;
+        document.getElementById('porcentaje').innerText = '12%';
+    }else if(valor === 'Sueldo'){
+        document.getElementById('porcentaje').value = 0.8;
+        document.getElementById('porcentaje').innerText = '8%';
+    }else if(valor === 'Negocio'){
+        document.getElementById('porcentaje').value = 0.10;
+        document.getElementById('porcentaje').innerText = '10%';
+    }
+}
+
+function procesar(){
+    var importe = document.getElementById('importe').value;
+    var porcentaje = document.getElementById('porcentaje').value;
+    var nombre = document.getElementById('nombre').value;
+    var cuotas = document.getElementById('cuotas').value;
+    if(porcentaje === "" || importe === ""){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Ingrese los valores faltantes',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true
+        })
+    }else if(nombre === "" || cuotas === ""){
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Ingrese los valores faltantes',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true
+        })
+    }else{
+        total_interes = importe * porcentaje;
+        total_pagar = Number(importe) + Number(total_interes);
+        document.getElementById('total_interes').value = Math.round(total_interes);
+        document.getElementById('total_pagar').value = Math.round(total_pagar);
+    }
+}
+
+function borrar(){
+    document.getElementById('nombre').value = '';
+    document.getElementById('importe').value = '';
+    document.getElementById('cuotas').value = '';
+    document.getElementById('total_interes').value = '';
+    document.getElementById('total_pagar').value = '';
+}
+
+document.getElementById('tipo_prestamo').addEventListener('select', calculo)
+document.getElementById('procesar').addEventListener('click', procesar)
+document.getElementById('borrar').addEventListener('click', borrar)
